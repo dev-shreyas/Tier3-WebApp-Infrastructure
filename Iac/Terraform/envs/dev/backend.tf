2eda04a929fb88f2616a388eb4f50ae066ba1460
Shreyas Basutkar <shriii.basutkar@gmail.com>
2026-02-24 15:59:34 +0530

    added dynamodb locks for s3 backend

--- Iac/Terraform/envs/dev/backend.tf
+++ Iac/Terraform/envs/dev/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "tf-state-managed-bucket-flaskapp"
-    key = "dev/eks/terraform.tfstate"
+    bucket = "flaskapp.tfstate"
+    key = "dev/mgmt/terraform.tfstate"
     region = "ap-southeast-2"
+    dynamodb_table = "tfstate-locks"
     use_lockfile = true
   }
 }
