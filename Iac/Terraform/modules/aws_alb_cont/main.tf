3f3051a8d807b3445703f04dce4ad483df1a8250
Shreyas Basutkar <shriii.basutkar@gmail.com>
2026-02-24 15:14:16 +0530

    Added ALB Controller helm chart to provision ingress load balancer

---
+++ Iac/Terraform/modules/aws_alb_cont/main.tf
@@ -0,0 +1,18 @@
+resource "helm_release" "alb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  values = [
+    yamlencode({
+      clusterName = var.cluster_name
+      region      = var.region
+      vpcId       = var.vpc_id
+      serviceAccount = {
+        create = false
+        name   = "aws-load-balancer-controller"
+      }
+    })
+  ]
+}
