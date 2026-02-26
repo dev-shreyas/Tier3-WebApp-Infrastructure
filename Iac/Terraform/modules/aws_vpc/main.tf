7ab444eeff68ecd7d138e61247bbd22b9823022a
Shreyas Basutkar <shriii.basutkar@gmail.com>
2026-02-26 14:24:59 +0530

    updated module vpc to auto- assign public ip to subnet

--- Iac/Terraform/modules/aws_vpc/main.tf
+++ Iac/Terraform/modules/aws_vpc/main.tf
@@ -8,6 +8,7 @@ module "vpc" {
   private_subnets = var.vpc_private_subnets
   public_subnets  = var.vpc_public_subnets
 
+  map_public_ip_on_launch = true
   enable_nat_gateway = var.enable_nat_gateway
   enable_vpn_gateway = var.enable_vpn_gateway
 }
