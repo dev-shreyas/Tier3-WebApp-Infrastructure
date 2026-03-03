fd933cefe4b7ed39b93aef61bd87e2fa1dfc1486
Shreyas Basutkar <shriii.basutkar@gmail.com>
2026-03-03 18:09:06 +0530

    Fixed ALB, IAM, VPC structure for provisioning components

---
+++ Iac/Terraform/envs/dev/provider.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>6.0"
+    }
+    helm = {
+        source = "hashicorp/helm"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.aws_managed_eks.cluster_name
+}
+
+provider "kubernetes" {
+  host = module.aws_managed_eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(
+    module.aws_managed_eks.cluster_certificate_authority_data
+  )
+    token = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes = {
+    host = module.aws_managed_eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(
+      module.aws_managed_eks.cluster_certificate_authority_data
+    )
+    token = data.aws_eks_cluster_auth.cluster.token
+  }
+}
